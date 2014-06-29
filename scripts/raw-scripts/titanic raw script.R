4c57c2f367e52c02be6901011d59986a5ba0a437
Wouter van Amstel <wlvanamstel@gmail.com>
2014-06-28 17:23:26 -0700

    Done with basic munging

--- scripts/raw-scripts/titanic raw script.R
+++ scripts/raw-scripts/titanic raw script.R
@@ -19,14 +19,17 @@ column.types <- c('integer', #passengerID
                  'factor' #port of embarcation
                  )
 missing.types <- c("NA","")
-ti_train <- readData('../../data/raw-data/train.csv', column.types, missing.types)
+orig_train <- readData('../../data/raw-data/train.csv', column.types, missing.types)
 column.types <- column.types[-2]
-ti_test <- readData('../../data/raw-data/test.csv', column.types, missing.types)
+orig_test <- readData('../../data/raw-data/test.csv', column.types, missing.types)
 
+ti_train <- orig_train
+ti_test <- orig_test
 #######################################################################
 ### examine the data
 #######################################################################
 require(ggplot2)
+require(caret)
 names(ti_train)
 str(ti_train)
 head(ti_train)
@@ -49,27 +52,147 @@ require(Amelia)    #plot missing values in data set
 missmap(df.train, main="Titanic Training Data - Missings Map", 
         col=c("yellow", "black"), legend=FALSE)
 
+#featureplot, not too informative
+featurePlot(x=ti_train[,c("Pclass", "Sex", "Age")], 
+            y=ti_train$Survived, plot="pairs")
+
+#young psng survive, higher fare means better chances? looks like it
+qplot(Fare, Age, colour=Survived, data=ti_train)
+cutAge <- cut(ti_train$Age, breaks=3)
+qplot(cutAge, Fare, data=ti_train, fill=cutAge, geom=c("boxplot"))
+table(cutAge, ti_train$Survived)
+#density plots
+qplot(Age, colour=Survived, data=ti_train, geom=c("density"))
+qplot(Age, colour=Pclass, data=ti_train, geom=c("density"))
+
 #######################################################################
 ### dealing with missing data points
 #######################################################################
 #only 2 missing values in the embarked feature
 ti_train$Embarked[which(is.na(ti_train$Embarked))] <- 'S' 
 
-#impute the missing ages based on titles
+#impute the missing ages
 summary(ti_train$Age)
+#replace missing values by average age
+# accuracy is 0.7857 in RF, slightly beter than without impute, but no
+#improvement on the test data
+ti_train$Age[which(is.na(ti_train$Age))] <- mean(ti_train$Age, na.rm=TRUE)
 
+#based on Class (see density plot), no dramatic improvement
+age_class1 <- mean(ti_train$Age[which(ti_train$Pclass==1)], na.rm=TRUE)
+age_class2 <- mean(ti_train$Age[which(ti_train$Pclass==2)], na.rm=TRUE)
+age_class3 <- mean(ti_train$Age[which(ti_train$Pclass==3)], na.rm=TRUE)
+ti_train$Age[which(ti_train$Pclass==1 & is.na(ti_train$Age))] <- age_class1
+ti_train$Age[which(ti_train$Pclass==2 & is.na(ti_train$Age))] <- age_class2
+ti_train$Age[which(ti_train$Pclass==3 & is.na(ti_train$Age))] <- age_class3
 
+#try to use titles as an indication of age
+getTitle <- function(data) {
+  title_start <- regexpr("\\,[A-Z ]{1,20}\\.", data$Name, TRUE)
+  title_end <- title_start + attr(title_start, "match.length")-1
+  data$Title <- substr(data$Name, title_start+2, title_end-1)
+  return (data$Title)
+}   
+ti_train$Title <- getTitle(ti_train)
+require(Hmisc)
+bystats(ti_train$Age, ti_train$Title,
+        fun=function(x) c(Mean=mean(x), Median=median(x)))
+
+missing_titles <- c('Dr', 'Master', 'Miss', 'Mr', 'Mrs')
+
+imputeMedian <- function(impute_var, filter_var, var_levels) {
+  for (v in var_levels) {
+    impute_var[which(filter_var == v)] <- impute(impute_var[ 
+      which(filter_var == v)])
+  }
+  return (impute_var)
+}
+#replace missing ages with median age based on title
+ti_train$Age <- imputeMedian(ti_train$Age, ti_train$Title, missing_titles)
+
+#investigate fare feature
+#replace 0 fares for median fare paid in applicable class
+ti_train$Fare[which(ti_train$Fare == 0)] <- NA
+ti_train$Fare <- imputeMedian(ti_train$Fare, ti_train$Pclass, 
+                              as.numeric(levels(ti_train$Pclass)))
+
+#investigate the title feature a little more
+ti_train$Title <- factor(ti_train$Title,
+                         c("Capt","Col","Major","Sir","Lady","Rev",
+                           "Dr","Don","Jonkheer","the Countess","Mrs",
+                           "Ms","Mr","Mme","Mlle","Miss","Master"))
+boxplot(ti_train$Age ~ ti_train$Title, 
+        main="Passenger Age by Title", xlab="Title", ylab="Age")
+
+## function for assigning a new title value to old title(s) 
+changeTitles <- function(data, old_titles, new_title) {
+  for (honorific in old_titles) {
+    data$Title[ which( data$Title == honorific)] <- new_title
+  }
+  return (data$Title)
+}
+
+#group the titles
+ti_train$Title <- changeTitles(ti_train, 
+                               c("Capt", "Col", "Don", "Dr", 
+                                 "Jonkheer", "Lady", "Major", 
+                                 "Rev", "Sir", "the Countess"),
+                               "Noble")
+ti_train$Title <- changeTitles(ti_train, c("Mlle", "Ms"), "Miss")
+ti_train$Title <- changeTitles(ti_train, c("Mme"), "Mrs")
+ti_train$Title <- as.factor(ti_train$Title)
+
+#more feature engineering?
+require(plyr)
+require(stringr)
+
+## test a character as an EVEN single digit
+isEven <- function(x) x %in% c("0","2","4","6","8") 
+## test a character as an ODD single digit
+isOdd <- function(x) x %in% c("1","3","5","7","9") 
+
+extraFeatures <- function(data){
+  data$Fate <- data$Survived
+  data$Fate <- revalue(data$Fate, c("1" = "Survived", "0" = "Dead"))
+  #women and children first in lifeboats
+  data$Priority <- "No"
+  data$Priority[which(data$Sex == "female" | data$Age < 16)] <- "Yes"
+  data$Priority <- as.factor(data$Priority)
+  #family = spouses/siblings + children
+  data$Family <- data$SibSp + data$Parch
+  #average fare
+  data$Fare_pp <- data$Fare/(data$Family + 1)
+  ## First character in Cabin number represents the Deck 
+  data$Deck <- substring(data$Cabin, 1, 1)
+  data$Deck[ which( is.na(data$Deck ))] <- "UNK"
+  data$Deck <- as.factor(data$Deck)
+  ## Odd-numbered cabins were reportedly on the port side of the ship
+  ## Even-numbered cabins assigned Side="starboard"
+  data$cabin.last.digit <- str_sub(data$Cabin, -1)
+  data$Side <- "UNK"
+  data$Side[which(isEven(data$cabin.last.digit))] <- "port"
+  data$Side[which(isOdd(data$cabin.last.digit))] <- "starboard"
+  data$Side <- as.factor(data$Side)
+  data$cabin.last.digit <- NULL
+  return (data)
+}
+
+ti_train <- extraFeatures(ti_train)
+
+features_to_use <- c("Fate", "Sex", "Priority", "Age", "Title", "Pclass",
+                     "Deck", "Side", "Fare", "Fare_pp", "Embarked", "Family")
+
+ti_train_mung <- ti_train[features_to_use]
 #######################################################################
 ### Simple regression tree
 #######################################################################
-require(caret)
 set.seed(23)
 in_train <- createDataPartition(y=ti_train$Survived, p=0.70, list=FALSE)
 training <- ti_train[in_train,]
 vali <- ti_train[-in_train,]
 
 ##fit regression tree model
-mod_rpart <- train(Survived ~ SibSp + Sex, method='rpart', data=training)
+mod_rpart <- train(Survived ~ Age + Embarked + Fare, method='rpart', data=training)
 print(mod_rpart$finalModel)
 require(rattle)
 fancyRpartPlot(mod_rpart$finalModel)
@@ -83,14 +206,13 @@ confusionMatrix(pred_rpart, vali$Survived)
 ##using plain random forest
 require(doMC)
 registerDoMC(2)
-tr_control <- trainControl(allowParallel=TRUE)
-mod_rf <- train(Survived ~ Sex + Pclass, method='rf', data=training)
+mod_rf <- train(Survived ~ Age + Sex + Embarked, method='rf', data=training)
 pred_rf <- predict(mod_rf, newdata=vali)
 confusionMatrix(pred_rf, vali$Survived)
 
 ## apply model to test data
 pred_rpart_test <- predict(mod_rpart, newdata=ti_test)
-Submission(ti_test$PassengerId, pred_rpart_test, "simpletree.cvs")
+Submission(ti_test$PassengerId, pred_rpart_test, "simplerf.cvs")
 
 #######################################################################
 ### Submissions
